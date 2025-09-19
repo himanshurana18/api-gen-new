1640732db22bbcdc9b2d877bc038b9829b712992
Himanshu rana <137643570+himanshurana18@users.noreply.github.com>
2025-09-19 16:23:57 +0530

    Initial commit or your message here

--- pages/manager/kaju/index.js
+++ pages/manager/kaju/index.js
@@ -1,7 +1,5 @@
-
-   import axios from "axios";
-   import Link from "next/link";
-   import { useState, useEffect, useRef } from "react";
-   import { FaPlus } from "react-icons/fa";
-   import { RiDeleteBinLine, RiEdit2Line } from "react-icons/ri";
-  
+import axios from "axios";
+import Link from "next/link";
+import { useState, useEffect, useRef } from "react";
+import { FaPlus } from "react-icons/fa";
+import { RiDeleteBinLine, RiEdit2Line } from "react-icons/ri";
